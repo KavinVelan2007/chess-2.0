dd722f4700a85c7623a4d66afaf2c65030e2ca80
useless-coder-person <gpranav1610@gmail.com>
2024-09-14 11:06:04 +0530

    occupancy done for use later

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from numpy import uint64 as uint
+import time
+import random
 from data import * 
 
 WHITE_PAWNS = uint(71776119061217280)
@@ -19,8 +21,6 @@ BLACK_BISHOPS = uint(36)
 
 def print_bitboard(bit_board):
     c = uint(0)
-    print('      A   B   C   D   E   F   G   H')
-    print('\n')
     
     for i in range(8):
         print(8 - i, '    ', end ='')
@@ -28,7 +28,11 @@ def print_bitboard(bit_board):
             print('1   ' if bit_board & (uint(1) << c) else '.   ', end = '')
             c += uint(1)
         print('\n')
+    print()        
+    print('      A   B   C   D   E   F   G   H')
+
     print('BitBoard Value:', bit_board)
+    print('\n')
 
 
 def generate_king_attacks():
@@ -114,3 +118,50 @@ def generate_bishop_occupancy():
             row -= 1
             col -= 1
         print(f'uint({n})',end=',')
+
+def count_bits(bit_board):
+    c = 0
+    while bit_board:
+        bit_board &= bit_board - uint(1)
+        c += 1
+    return c
+
+
+def least_significant_bit_count(bit_board):
+    return(count_bits(bit_board - uint(1)) - count_bits(bit_board) + 1)
+
+
+def fill_occupancy(value, bit_board):
+    occupancy = uint(0)
+    c = uint(0)
+    while bit_board:
+        if value & (uint(1) << c):
+            occupancy |= uint(1) << uint(count_bits(bit_board - uint(1)) - count_bits(bit_board) + uint(1))
+        bit_board &= bit_board - uint(1)
+        c += uint(1)
+    return occupancy
+
+
+def random_number_generate():
+    seed = uint(time.time())
+    
+    seed ^= seed << uint(15)
+    seed ^= seed >> uint(47)
+    seed ^= seed << uint(45)
+    seed ^= seed >> uint(10)
+    seed ^= seed << uint(55)
+    seed ^= seed >> uint(49)
+    
+    return seed
+
+
+'''
+def generate_magic_numbers(position, bits_present, piece): # For the piece parameter, bishop = 0, rook = 1
+    occupancy = []
+    attack = []
+    used_attacks = []
+    for i in range(1 << (ROOK_OCCUPANCY_BITS[position] if piece else BISHOP_OCCUPANCY_BITS[position])):
+        occupancy += [fill_occupancy(uint(i), ROOK_OCCUPANCY[position])]
+'''
+
+
