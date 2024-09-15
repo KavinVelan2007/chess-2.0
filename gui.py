a98f3c4661dbd3b5e3ad5ac4ae2e64aeb8412d3f
KavinVelan2007 <kavinvelan4@gmail.com>
2024-09-15 11:02:29 +0530

    Added Basic Components in GUI

---
+++ gui.py
@@ -0,0 +1,31 @@
+import pygame
+from data import *
+
+display = pygame.display.set_mode((0,0),pygame.FULLSCREEN)
+
+WINDOW_WIDTH,WINDOW_HEIGHT = pygame.display.Info().current_w,pygame.display.Info().current_h
+BOARD_WIDTH,BOARD_HEIGHT = WINDOW_HEIGHT,WINDOW_HEIGHT
+
+def display_board(display):
+    for row in range(8):
+        for col in range(8):
+            if (row + col) % 2:
+                pygame.draw.rect(display,(255,255,255),(WINDOW_WIDTH // 2 - WINDOW_HEIGHT // 2 + col * (BOARD_WIDTH // 8),row * (BOARD_HEIGHT // 8),BOARD_WIDTH // 8,BOARD_HEIGHT // 8))
+            else:
+                pygame.draw.rect(display,(114,149,83),(WINDOW_WIDTH // 2 - WINDOW_HEIGHT // 2 + col * (BOARD_WIDTH // 8),row * (BOARD_HEIGHT // 8),BOARD_WIDTH // 8,BOARD_HEIGHT // 8))
+            
+def run():
+    run = True
+    while run:
+        for event in pygame.event.get():
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_q:
+                    run = False
+                    
+        display.fill((30,30,30))
+        display_board(display)
+        pygame.display.update()
+        
+    pygame.quit()
+
+run()
--- main.py
+++ main.py
@@ -5,22 +5,6 @@ import random
 random.seed(time.time())
 from data import * 
 
-WHITE_PAWNS = uint(71776119061217280)
-WHITE_KING = uint(576460752303423488)
-WHITE_QUEEN = uint(1152921504606846976)
-WHITE_ROOKS = uint(9295429630892703744)
-WHITE_KNIGHTS = uint(4755801206503243776)
-WHITE_BISHOPS = uint(2594073385365405696)
-
-
-BLACK_PAWNS = uint(65280)
-BLACK_KING = uint(8)
-BLACK_QUEEN = uint(16)
-BLACK_ROOKS = uint(129)
-BLACK_KNIGHTS = uint(66)
-BLACK_BISHOPS = uint(36)
-
-
 def print_bitboard(bit_board):
     c = uint(0)
     
