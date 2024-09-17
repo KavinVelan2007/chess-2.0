e821286666cc57d5fe8170116f6a5e6f5bbaf357
KavinVelan2007 <kavinvelan4@gmail.com>
2024-09-17 21:05:57 +0530

    Repositioned the board

--- constants.py
+++ constants.py
@@ -3,4 +3,4 @@ import pygame
 display = pygame.display.set_mode((0,0),pygame.FULLSCREEN)
 
 WINDOW_WIDTH,WINDOW_HEIGHT = pygame.display.Info().current_w,pygame.display.Info().current_h
-BOARD_WIDTH,BOARD_HEIGHT = WINDOW_HEIGHT,WINDOW_HEIGHT
+BOARD_WIDTH,BOARD_HEIGHT = WINDOW_HEIGHT - 150,WINDOW_HEIGHT - 150
