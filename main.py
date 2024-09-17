293e8704d5b824bc1a2bfbd8f9c88171e2f2e30c
KavinVelan2007 <kavinvelan4@gmail.com>
2024-09-17 21:45:52 +0530

    Dummy Commit

--- main.py
+++ main.py
@@ -1,25 +1,27 @@
-from numpy import uint64 as uint, uint32, array,set_printoptions
+from numpy import uint64 as uint, uint32, array, set_printoptions
 import time
 import sys
 import random
+
 random.seed(time.time())
-from data import * 
+from data import *
 
 set_printoptions(threshold=sys.maxsize)
+
 
 def print_bitboard(bit_board):
     c = uint(0)
     for i in range(8):
-        print(8 - i, '    ', end ='')
+        print(8 - i, "    ", end="")
         for j in range(8):
-            print('1   ' if bit_board & (uint(1) << c) else '.   ', end = '')
+            print("1   " if bit_board & (uint(1) << c) else ".   ", end="")
             c += uint(1)
-        print('\n')
-    print()        
-    print('      A   B   C   D   E   F   G   H')
+        print("\n")
     print()
-    print('BitBoard Value:', bit_board)
-    print('\n')
+    print("      A   B   C   D   E   F   G   H")
+    print()
+    print("BitBoard Value:", bit_board)
+    print("\n")
 
 
 def generate_king_attacks():
@@ -41,8 +43,8 @@ def generate_king_attacks():
             c |= uint(1) << uint(i - 8)
         if i <= 55:
             c |= uint(1) << uint(i + 8)
-        print('uint(', c, '),', sep = '')
-        
+        print("uint(", c, "),", sep="")
+
 
 def generate_knight_attacks():
     for i in range(64):
@@ -61,50 +63,52 @@ def generate_knight_attacks():
             if i >= 16:
                 c |= uint(1) << uint(i - 15)
             if i <= 47:
-                c |= uint(1) << uint(i  + 17)
+                c |= uint(1) << uint(i + 17)
             if i % 8 <= 5:
                 if i >= 8:
                     c |= uint(1) << uint(i - 6)
                 if i <= 55:
                     c |= uint(1) << uint(i + 10)
-        print('uint(', c, '),', sep = '')
-        
+        print("uint(", c, "),", sep="")
+
 
 def generate_rook_occupancy():
     for i in range(64):
         c = uint(0)
-        for j in [1,2,3,4,5,6]:
+        for j in [1, 2, 3, 4, 5, 6]:
             if i % 8 != j:
                 c |= uint(1) << uint(i - i % 8 + j)
             if i // 8 != j:
                 c |= uint(1) << uint(j * 8 + i % 8)
-        print('uint(', c, '),', sep = '')
+        print("uint(", c, "),", sep="")
+
 
 def generate_bishop_occupancy():
     for i in range(64):
         n = uint(0)
-        r,c = i // 8,i % 8
-        row,col = r + 1,c + 1
+        r, c = i // 8, i % 8
+        row, col = r + 1, c + 1
         while row <= 6 and col <= 6:
             n |= uint(1) << uint(row * 8 + col)
             row += 1
             col += 1
-        row,col = r - 1,c + 1
+        row, col = r - 1, c + 1
         while row >= 1 and col <= 6:
             n |= uint(1) << uint(row * 8 + col)
             row -= 1
             col += 1
-        row,col = r + 1,c - 1
+        row, col = r + 1, c - 1
         while row <= 6 and col >= 1:
             n |= uint(1) << uint(row * 8 + col)
             row += 1
             col -= 1
-        row,col = r - 1,c - 1
+        row, col = r - 1, c - 1
         while row >= 1 and col >= 1:
             n |= uint(1) << uint(row * 8 + col)
             row -= 1
             col -= 1
-        print(f'uint({n})',end=',')
+        print(f"uint({n})", end=",")
+
 
 def count_bits(bit_board):
     c = 0
@@ -115,7 +119,8 @@ def count_bits(bit_board):
 
 
 def least_significant_bit_count(bit_board):
-    return(count_bits(bit_board - uint(1)) - count_bits(bit_board) + 1)
+    return count_bits(bit_board - uint(1)) - count_bits(bit_board) + 1
+
 
 def most_significant_bit_count(bitboard):
     ndx = 0
@@ -125,54 +130,64 @@ def most_significant_bit_count(bitboard):
         ndx += 1
     return ndx
 
+
 def fill_occupancy(value, bit_board):
     occupancy = uint(0)
     c = uint(0)
     while bit_board:
         if value & (uint(1) << c):
-            occupancy |= uint(1) << uint(count_bits(bit_board - uint(1)) - count_bits(bit_board) + uint(1))
+            occupancy |= uint(1) << uint(
+                count_bits(bit_board - uint(1)) - count_bits(bit_board) + uint(1)
+            )
         bit_board &= bit_board - uint(1)
         c += uint(1)
     return occupancy
 
 
-
 seed = uint32(time.time())
-def random_number_generate(): 
+
+
+def random_number_generate():
     def rand():
-       global seed
-       seed ^= seed << uint32(13)
-       seed ^= seed >> uint32(17)
-       seed ^= seed << uint32(5)     
-       return seed
-    return ((uint(rand() & uint32(0xFFFF))) | (uint(rand() & uint32(0xFFFF)) << uint(16)) | (uint(rand() & uint32(0xFFFF)) << uint(32)) | (uint(rand() & uint32(0xFFFF)) << uint(48)))
+        global seed
+        seed ^= seed << uint32(13)
+        seed ^= seed >> uint32(17)
+        seed ^= seed << uint32(5)
+        return seed
+
+    return (
+        (uint(rand() & uint32(0xFFFF)))
+        | (uint(rand() & uint32(0xFFFF)) << uint(16))
+        | (uint(rand() & uint32(0xFFFF)) << uint(32))
+        | (uint(rand() & uint32(0xFFFF)) << uint(48))
+    )
 
 
-def generate_bishop_attacks(i,mask):
+def generate_bishop_attacks(i, mask):
     n = uint(0)
-    r,c = i // 8,i % 8
-    row,col = r + 1,c + 1
+    r, c = i // 8, i % 8
+    row, col = r + 1, c + 1
     while row <= 7 and col <= 7:
         n |= uint(1) << uint(row * 8 + col)
         if mask & (uint(1) << uint(row * 8 + col)):
             break
         row += 1
         col += 1
-    row,col = r - 1,c + 1
+    row, col = r - 1, c + 1
     while row >= 0 and col <= 7:
         n |= uint(1) << uint(row * 8 + col)
         if mask & (uint(1) << uint(row * 8 + col)):
             break
         row -= 1
         col += 1
-    row,col = r + 1,c - 1
+    row, col = r + 1, c - 1
     while row <= 7 and col >= 0:
         n |= uint(1) << uint(row * 8 + col)
         if mask & (uint(1) << uint(row * 8 + col)):
             break
         row += 1
         col -= 1
-    row,col = r - 1,c - 1
+    row, col = r - 1, c - 1
     while row >= 0 and col >= 0:
         n |= uint(1) << uint(row * 8 + col)
         if mask & (uint(1) << uint(row * 8 + col)):
@@ -181,49 +196,63 @@ def generate_bishop_attacks(i,mask):
         col -= 1
     return n
 
-def generate_rook_attacks(i,mask):
+
+def generate_rook_attacks(i, mask):
     row = i // 8
     col = i % 8
     n = uint(0)
-    for r in range(row + 1,8):
+    for r in range(row + 1, 8):
         n |= uint(1) << uint(r * 8 + col)
         if mask & (uint(1) << uint(r * 8 + col)):
             break
-    for r in range(row - 1,-1,-1):
+    for r in range(row - 1, -1, -1):
         n |= uint(1) << uint(r * 8 + col)
         if mask & (uint(1) << uint(r * 8 + col)):
             break
-    for c in range(col + 1,8):
+    for c in range(col + 1, 8):
         n |= uint(1) << uint(row * 8 + c)
         if mask & (uint(1) << uint(row * 8 + c)):
             break
-    for c in range(col - 1,-1,-1):
+    for c in range(col - 1, -1, -1):
         n |= uint(1) << uint(row * 8 + c)
         if mask & (uint(1) << uint(row * 8 + c)):
             break
     return n
 
 
-def generate_magic_numbers(position, bits_present, piece): # For the piece parameter, bishop = 0, rook = 1
+def generate_magic_numbers(
+    position, bits_present, piece
+):  # For the piece parameter, bishop = 0, rook = 1
     occupancy = []
     attack = []
     used_attacks = []
-    current_occupancy = ROOK_OCCUPANCY[position] if piece else BISHOP_OCCUPANCY[position]
+    current_occupancy = (
+        ROOK_OCCUPANCY[position] if piece else BISHOP_OCCUPANCY[position]
+    )
     for i in range(uint(1) << uint(bits_present)):
         occupancy += [fill_occupancy(uint(i), current_occupancy)]
-        attack += [generate_rook_attacks(position, occupancy[i]) if piece else generate_bishop_attacks(position, occupancy[i])]
-        
+        attack += [
+            generate_rook_attacks(position, occupancy[i])
+            if piece
+            else generate_bishop_attacks(position, occupancy[i])
+        ]
+
     for i in range(100000000):
-        magic_number = uint(random.randint(0, (1 << 64) - 1)) & uint(random.randint(0, (1 << 64) - 1))   
-        if count_bits((current_occupancy * magic_number) & uint(0xFF00000000000000)) < 6:
+        magic_number = uint(random.randint(0, (1 << 64) - 1)) & uint(
+            random.randint(0, (1 << 64) - 1)
+        )
+        if (
+            count_bits((current_occupancy * magic_number) & uint(0xFF00000000000000))
+            < 6
+        ):
             continue
         used_attacks = [uint(0) for i in range(uint(1) << uint(bits_present))]
-        
+
         index = 0
         failed = False
-        
+
         while not failed and index < (uint(1) << uint(bits_present)):
-            magic_index = ((occupancy[index] * magic_number) >> uint(64 - bits_present))
+            magic_index = (occupancy[index] * magic_number) >> uint(64 - bits_present)
             if used_attacks[magic_index] == uint(0):
                 used_attacks[magic_index] = attack[index]
             elif used_attacks[magic_index] != attack[index]:
@@ -240,31 +269,68 @@ def init_magic_tables():
     BISHOP_ATTACKS = array([[uint(0) for x in range(1 << 9)] for i in range(64)])
     for i in range(64):
         for j in range(1 << ROOK_OCCUPANCY_BITS[i]):
-            magic_index = ((fill_occupancy(uint(j), ROOK_OCCUPANCY[i]) * ROOK_MAGIC_NUMBERS[i]) >> uint(64 - ROOK_OCCUPANCY_BITS[i]))
+            magic_index = (
+                fill_occupancy(uint(j), ROOK_OCCUPANCY[i]) * ROOK_MAGIC_NUMBERS[i]
+            ) >> uint(64 - ROOK_OCCUPANCY_BITS[i])
             if ROOK_ATTACKS[i][magic_index] == uint(0):
-                ROOK_ATTACKS[i][magic_index] = generate_rook_attacks(i, fill_occupancy(uint(j), ROOK_OCCUPANCY[i]))
-            elif ROOK_ATTACKS[i][magic_index] != generate_rook_attacks(i, fill_occupancy(uint(j), ROOK_OCCUPANCY[i])):
-                raise ValueError('Magic Number is not so magical!')
+                ROOK_ATTACKS[i][magic_index] = generate_rook_attacks(
+                    i, fill_occupancy(uint(j), ROOK_OCCUPANCY[i])
+                )
+            elif ROOK_ATTACKS[i][magic_index] != generate_rook_attacks(
+                i, fill_occupancy(uint(j), ROOK_OCCUPANCY[i])
+            ):
+                raise ValueError("Magic Number is not so magical!")
         for j in range(1 << BISHOP_OCCUPANCY_BITS[i]):
-            magic_index = ((fill_occupancy(uint(j), BISHOP_OCCUPANCY[i]) * BISHOP_MAGIC_NUMBERS[i]) >> uint(64 - BISHOP_OCCUPANCY_BITS[i]))
+            magic_index = (
+                fill_occupancy(uint(j), BISHOP_OCCUPANCY[i]) * BISHOP_MAGIC_NUMBERS[i]
+            ) >> uint(64 - BISHOP_OCCUPANCY_BITS[i])
             if BISHOP_ATTACKS[i][magic_index] == uint(0):
-                BISHOP_ATTACKS[i][magic_index] = generate_bishop_attacks(i, fill_occupancy(uint(j), BISHOP_OCCUPANCY[i]))
-            elif BISHOP_ATTACKS[i][magic_index] != generate_bishop_attacks(i, fill_occupancy(uint(j), BISHOP_OCCUPANCY[i])):
-                raise ValueError('Magic Number is not so magical!') 
+                BISHOP_ATTACKS[i][magic_index] = generate_bishop_attacks(
+                    i, fill_occupancy(uint(j), BISHOP_OCCUPANCY[i])
+                )
+            elif BISHOP_ATTACKS[i][magic_index] != generate_bishop_attacks(
+                i, fill_occupancy(uint(j), BISHOP_OCCUPANCY[i])
+            ):
+                raise ValueError("Magic Number is not so magical!")
 
 
 def generate_rook_moves(square, attack_mask):
-    return ROOK_ATTACKS[square][(((ROOK_OCCUPANCY[square] & attack_mask) * ROOK_MAGIC_NUMBERS[square]) >> uint(64 - ROOK_OCCUPANCY_BITS[square]))]
+    return ROOK_ATTACKS[square][
+        (
+            ((ROOK_OCCUPANCY[square] & attack_mask) * ROOK_MAGIC_NUMBERS[square])
+            >> uint(64 - ROOK_OCCUPANCY_BITS[square])
+        )
+    ]
 
 
 def generate_bishop_moves(square, attack_mask):
-    return BISHOP_ATTACKS[square][(((BISHOP_OCCUPANCY[square] & attack_mask) * BISHOP_MAGIC_NUMBERS[square]) >> uint(64 - BISHOP_OCCUPANCY_BITS[square]))]
+    return BISHOP_ATTACKS[square][
+        (
+            ((BISHOP_OCCUPANCY[square] & attack_mask) * BISHOP_MAGIC_NUMBERS[square])
+            >> uint(64 - BISHOP_OCCUPANCY_BITS[square])
+        )
+    ]
+
 
 def return_moves(square):
-    WHITE_PIECES = WHITE_BISHOPS | WHITE_KING | WHITE_KNIGHTS | WHITE_PAWNS | WHITE_QUEEN | WHITE_ROOKS
-    BLACK_PIECES = BLACK_BISHOPS | BLACK_KING | BLACK_KNIGHTS | BLACK_PAWNS | BLACK_QUEEN | BLACK_ROOKS
+    WHITE_PIECES = (
+        WHITE_BISHOPS
+        | WHITE_KING
+        | WHITE_KNIGHTS
+        | WHITE_PAWNS
+        | WHITE_QUEEN
+        | WHITE_ROOKS
+    )
+    BLACK_PIECES = (
+        BLACK_BISHOPS
+        | BLACK_KING
+        | BLACK_KNIGHTS
+        | BLACK_PAWNS
+        | BLACK_QUEEN
+        | BLACK_ROOKS
+    )
     if BLACK_PAWNS & (uint(1) << uint(square)):
-        res = (BLACK_PAWN_ATTACKS[square] & WHITE_PIECES)
+        res = BLACK_PAWN_ATTACKS[square] & WHITE_PIECES
         temp = BLACK_PAWN_PUSHES[square]
         while temp:
             index = least_significant_bit_count(temp)
@@ -275,7 +341,7 @@ def return_moves(square):
             temp &= temp - uint(1)
         return res
     elif WHITE_PAWNS & (uint(1) << uint(square)):
-        res = (WHITE_PAWN_ATTACKS[square] & BLACK_PIECES)
+        res = WHITE_PAWN_ATTACKS[square] & BLACK_PIECES
         temp = WHITE_PAWN_PUSHES[square]
         while temp:
             index = most_significant_bit_count(temp)
@@ -283,10 +349,18 @@ def return_moves(square):
                 res |= uint(1) << uint(index)
             else:
                 break
-            temp -= (uint(1) << uint(index))
+            temp -= uint(1) << uint(index)
         return res
     elif (BLACK_BISHOPS | WHITE_BISHOPS) & (uint(1) << uint(square)):
-        moves = BISHOP_ATTACKS[square][(((BISHOP_OCCUPANCY[square] & (BLACK_PIECES | WHITE_PIECES)) * BISHOP_MAGIC_NUMBERS[square]) >> uint(64 - BISHOP_OCCUPANCY_BITS[square]))]
+        moves = BISHOP_ATTACKS[square][
+            (
+                (
+                    (BISHOP_OCCUPANCY[square] & (BLACK_PIECES | WHITE_PIECES))
+                    * BISHOP_MAGIC_NUMBERS[square]
+                )
+                >> uint(64 - BISHOP_OCCUPANCY_BITS[square])
+            )
+        ]
         if BLACK_BISHOPS & (uint(1) << uint(square)):
             friendly_attacks = moves & BLACK_PIECES
         else:
@@ -302,7 +376,15 @@ def return_moves(square):
         moves &= ~friendly_attacks
         return moves
     elif (BLACK_ROOKS | WHITE_ROOKS) & (uint(1) << uint(square)):
-        moves = ROOK_ATTACKS[square][(((ROOK_OCCUPANCY[square] & (BLACK_PIECES | WHITE_PIECES)) * ROOK_MAGIC_NUMBERS[square]) >> uint(64 - ROOK_OCCUPANCY_BITS[square]))]
+        moves = ROOK_ATTACKS[square][
+            (
+                (
+                    (ROOK_OCCUPANCY[square] & (BLACK_PIECES | WHITE_PIECES))
+                    * ROOK_MAGIC_NUMBERS[square]
+                )
+                >> uint(64 - ROOK_OCCUPANCY_BITS[square])
+            )
+        ]
         if BLACK_ROOKS & (uint(1) << uint(square)):
             friendly_attacks = moves & BLACK_PIECES
         else:
@@ -310,8 +392,24 @@ def return_moves(square):
         moves &= ~friendly_attacks
         return moves
     elif (BLACK_QUEEN | WHITE_QUEEN) & (uint(1) << uint(square)):
-        rook_moves = ROOK_ATTACKS[square][(((ROOK_OCCUPANCY[square] & (BLACK_PIECES | WHITE_PIECES)) * ROOK_MAGIC_NUMBERS[square]) >> uint(64 - ROOK_OCCUPANCY_BITS[square]))]
-        bishop_moves = BISHOP_ATTACKS[square][(((BISHOP_OCCUPANCY[square] & (BLACK_PIECES | WHITE_PIECES)) * BISHOP_MAGIC_NUMBERS[square]) >> uint(64 - BISHOP_OCCUPANCY_BITS[square]))]
+        rook_moves = ROOK_ATTACKS[square][
+            (
+                (
+                    (ROOK_OCCUPANCY[square] & (BLACK_PIECES | WHITE_PIECES))
+                    * ROOK_MAGIC_NUMBERS[square]
+                )
+                >> uint(64 - ROOK_OCCUPANCY_BITS[square])
+            )
+        ]
+        bishop_moves = BISHOP_ATTACKS[square][
+            (
+                (
+                    (BISHOP_OCCUPANCY[square] & (BLACK_PIECES | WHITE_PIECES))
+                    * BISHOP_MAGIC_NUMBERS[square]
+                )
+                >> uint(64 - BISHOP_OCCUPANCY_BITS[square])
+            )
+        ]
         moves = rook_moves | bishop_moves
         if BLACK_QUEEN & (uint(1) << uint(square)):
             friendly_attacks = moves & BLACK_PIECES
@@ -327,8 +425,10 @@ def return_moves(square):
             friendly_attacks = moves & WHITE_PIECES
         moves &= ~friendly_attacks
         return moves
-    
-'''
+    else:
+        return uint(0)
+
+"""
 BOARD REPRESENTATION
 12 bitboards for positions in order of 'PNBRQKpnbrqk'
 
@@ -338,12 +438,12 @@ first bit = 0 if white to play 1 if black to play
 next four bits for castling data = KQkq
 next bit = enpassant possible or not
 next 6 bits = enpassant square
-'''
-pieces = 'PNBRQKpnbrqk'
+"""
+pieces = "PNBRQKpnbrqk"
 
 board = BITBOARDS
 board_data = uint32(0b01111000000000000000000000000000)
-'''
+"""
 def print_chess_board(boards, board_data):
     c = uint(0)
     
@@ -363,4 +463,4 @@ def print_chess_board(boards, board_data):
     print(f'Side to move: {}')
     print()
     print(f'En Passant: {}')
-    print('\n')'''
+    print('\n')"""
