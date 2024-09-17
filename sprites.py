54a9ad2a7e09eb7017fcb86c01fcf16cb3428ce3
KavinVelan2007 <kavinvelan4@gmail.com>
2024-09-17 21:49:58 +0530

    Windows Problem Solved

--- sprites.py
+++ sprites.py
@@ -4,4 +4,4 @@ from constants import *
 sprites = {}
 
 for img_name in os.listdir('sprites'):
-    sprites[img_name[0]] = pygame.transform.scale(pygame.image.load(f'sprites/{img_name}'),(BOARD_WIDTH // 8,BOARD_HEIGHT // 8))
+    sprites[img_name[1]] = pygame.transform.scale(pygame.image.load(f'sprites/{img_name}'),(BOARD_WIDTH // 8,BOARD_HEIGHT // 8))
