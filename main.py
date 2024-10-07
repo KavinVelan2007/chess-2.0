977a7820011cd556d5931860aeb849b9d78ebb7b
useless-coder-person <gpranav1610@gmail.com>
2024-10-07 06:59:48 +0530

    Finished move generation function

--- main.py
+++ main.py
@@ -489,10 +489,10 @@ def return_moves(side, bitboards, board_data):
                 attacks &= (attacks - uint(1))
                 add_move(curr_square, to_square, 5, 0, 0, 0, 0)
 
-            if board_data & uint32(2) and not (ALL_PIECES & uint((1 << 62) + (1 << 61))):
+            if board_data & uint32(2) and not (ALL_PIECES & uint((1 << 62) + (1 << 61))) and not is_square_atacked(1, 61, bitboards , board_data, BLACK_PIECES, WHITE_PIECES) and not is_square_atacked(1, 60, bitboards , board_data, BLACK_PIECES, WHITE_PIECES):
                 add_move(curr_square, 62, 5, 0, 0, 0, 1)
 
-            if board_data & uint32(4) and not (ALL_PIECES & uint((1 << 59) + (1 << 58) + (1 << 57))):
+            if board_data & uint32(4) and not (ALL_PIECES & uint((1 << 59) + (1 << 58) + (1 << 57))) and not is_square_atacked(1, 59, bitboards , board_data, BLACK_PIECES, WHITE_PIECES) and not is_square_atacked(1, 60, bitboards , board_data, BLACK_PIECES, WHITE_PIECES):
                 add_move(curr_square, 58, 5, 0, 0, 0, 1)
 
 
@@ -622,10 +622,10 @@ def return_moves(side, bitboards, board_data):
                 attacks &= (attacks - uint(1))
                 add_move(curr_square, to_square, 11, 0, 0, 0, 0)
 
-            if board_data & uint32(8) and not (ALL_PIECES & uint((1 << 5) + (1 << 6))):
+            if board_data & uint32(8) and not (ALL_PIECES & uint((1 << 5) + (1 << 6))) and not is_square_atacked(0, 3, bitboards , board_data, BLACK_PIECES, WHITE_PIECES) and not is_square_atacked(0, 4, bitboards , board_data, BLACK_PIECES, WHITE_PIECES):
                 add_move(curr_square, 6, 11, 0, 0, 0, 1)
 
-            if board_data & uint32(16) and not (ALL_PIECES & uint((1 << 1) + (1 << 2) + (1 << 3))):
+            if board_data & uint32(16) and not (ALL_PIECES & uint((1 << 1) + (1 << 2) + (1 << 3))) and not is_square_atacked(0, 4, bitboards , board_data, BLACK_PIECES, WHITE_PIECES) and not is_square_atacked(0, 5, bitboards , board_data, BLACK_PIECES, WHITE_PIECES):
                 add_move(curr_square, 2, 11, 0, 0, 0, 1)
 
 
@@ -647,7 +647,7 @@ def make_move(move, bitboards, data):
     # bitboards format: list[P,N,B,R,Q,K,p,n,b,r,q,k]
     # from_,to: tuple(row,col)
     # data: uint64 (format for data given below this fn)
-    piece = (move << uint32(12)) & uint32(15)
+    piece = (move >> uint32(12)) & uint32(15)
     source = move & uint32(63)
     target = (move >> uint32(6)) & uint32(63)
 
@@ -659,11 +659,12 @@ def make_move(move, bitboards, data):
         if (move >> uint32(16)) & uint32(1):
             bitboards[0] ^= uint(1 << source)
             bitboards[((move >> uint32(17)) & uint32(3)) + 1] |= uint(1 << target)
-            # to add capture and change half move clock
+            data &= uint32(4095)
 
         elif (move << uint32(19)) & uint32(1):
             bitboards[0] ^= uint((1 << source) + (1 << target))
             bitboards[6] ^= uint(1 << (target + 8))
+            data &= uint32(4095)
 
         elif (move << uint32(19)) & uint32(1):
             bitboards[5] ^= uint((1 << source) + (1 << target))
@@ -675,19 +676,92 @@ def make_move(move, bitboards, data):
             else:
                 bitboards[3] ^= uint(1 << 56)
 
+            data += uint32(4096)
+
         else:
             bitboards[piece] ^= uint((1 << source) + (1 << target))
-            bitboards[6] &= ~uint(1 << target)
-            bitboards[7] &= ~uint(1 << target)
-            bitboards[8] &= ~uint(1 << target)
-            bitboards[9] &= ~uint(1 << target)
-            bitboards[10] &= ~uint(1 << target)
 
+            mask = uint(0)
+            for i in range(6):
+                mask |= bitboards[6 + i]
+                bitboards[i + 6] &= ~uint(1 << target)
+
+            if mask & uint(1 << target):
+                data &= uint32(4095)
+            else:
+                data += uint32(4096)
 
         data |= uint32(1)
 
+        if piece == 0 and source - target == 16:
+            data &= ~uint32(((1 << 6) - 1) << 6)
+            data |= uint32((target + 8) << 6)
+
+        if piece == 5:
+            data &= ~uint32(6)
+
+        if data & uint32(2) and piece == 3 and source == 63:
+            data &= ~uint32(2)
+
+        elif data & uint32(4) and piece == 3 and source == 56:
+            data &= ~uint32(4)
+
+        if is_square_atacked(1, least_significant_bit_count(bitboards[5]), bitboards, data, bitboards[6] | bitboards[7] | bitboards[8] | bitboards[9] | bitboards[10] | bitboards[11], bitboards[0] | bitboards[1] | bitboards[2] | bitboards[3] | bitboards[4] | bitboards[5]):
+            return False
+
     else:
-        pass
+        if (move >> uint32(16)) & uint32(1):
+            bitboards[6] ^= uint(1 << source)
+            bitboards[((move >> uint32(17)) & uint32(3)) + 7] |= uint(1 << target)
+            data &= uint32(4095)
+
+        elif (move << uint32(19)) & uint32(1):
+            bitboards[6] ^= uint((1 << source) + (1 << target))
+            bitboards[0] ^= uint(1 << (target + 8))
+            data &= uint32(4095)
+
+        elif (move << uint32(19)) & uint32(1):
+            bitboards[11] ^= uint((1 << source) + (1 << target))
+            bitboards[9] ^= uint(1 << ((source + target) // 2))
+
+            if target == 2:
+                bitboards[9] ^= uint(1 << 3)
+            
+            else:
+                bitboards[9] ^= uint(1 << 5)
+
+        else:
+            bitboards[piece] ^= uint((1 << source) + (1 << target))
+
+            mask = uint(0)
+            for i in range(6):
+                mask |= bitboards[i]
+                bitboards[i] &= ~uint(1 << target)
+
+            if mask & uint(1 << target):
+                data &= uint32(4095)
+            else:
+                data += uint32(4096)
+
+        data ^= uint32(1)
+
+        if piece == 6 and target - source == 16:
+            data &= ~uint32(((1 << 6) - 1) << 6)
+            data |= uint32((target - 8) << 6)
+
+        if piece == 11:
+            data &= ~uint32(24)
+
+        if data & uint32(8) and piece == 9 and source == 7:
+            data &= ~uint32(8)
+
+        elif data & uint32(16) and piece == 9 and source == 0:
+            data &= ~uint32(16)
+
+        if is_square_atacked(0, least_significant_bit_count(bitboards[11]), bitboards, data, bitboards[6] | bitboards[7] | bitboards[8] | bitboards[9] | bitboards[10] | bitboards[11], bitboards[0] | bitboards[1] | bitboards[2] | bitboards[3] | bitboards[4] | bitboards[5]):
+            return False
+
+    return bitboards, data
     
 
 """
@@ -738,10 +812,26 @@ def print_chess_board(boards, board_data):
 bitboards = array(BITBOARDS.copy())
 board_data = data
 
+def perft():
+    pass
+
 
 moves = return_moves(0, bitboards, board_data)
 
-print_moves(moves)
+for move in moves:
+    a = make_move(move, bitboards.copy(), board_data)
+    if a == False:
+        continue
+    else:
+        temp_bitboards, temp_board_data = a
 
-print(len(moves))
+    moves2 = return_moves(1, temp_bitboards, temp_board_data)
 
+    for move2 in moves2:
+        b = make_move(move2, temp_bitboards.copy(), temp_board_data)
+        if b == False:
+            continue
+        else:
+            temp_temp_bitboards, temp_temp_board_data = b
+        print_chess_board(temp_temp_bitboards, temp_temp_board_data)
+        input()
